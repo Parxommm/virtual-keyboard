42889099b3bcf00e652319aa8aaa94fe2f2fea48
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2022-05-03 17:55:48 +0300

    feat: create keys list

---
+++ virtual-keyboard/js/keys.js
@@ -0,0 +1,575 @@
+const keys = {
+  Backquote: {
+    code: 'Backquote',
+    en: '`',
+    ru: 'ё',
+    shiftEn: '~',
+    shiftRu: 'Ё',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit1: {
+    code: 'Digit1',
+    en: '1',
+    ru: '1',
+    shiftEn: '!',
+    shiftRu: '!',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit2: {
+    code: 'Digit2',
+    en: '2',
+    ru: '2',
+    shiftEn: '@',
+    shiftRu: '"',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit3: {
+    code: 'Digit3',
+    en: '3',
+    ru: '3',
+    shiftEn: '#',
+    shiftRu: '№',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit4: {
+    code: 'Digit4',
+    en: '4',
+    ru: '4',
+    shiftEn: '$',
+    shiftRu: ';',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit5: {
+    code: 'Digit5',
+    en: '5',
+    ru: '5',
+    shiftEn: '%',
+    shiftRu: '%',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit6: {
+    code: 'Digit6',
+    en: '6',
+    ru: '6',
+    shiftEn: '^',
+    shiftRu: ':',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit7: {
+    code: 'Digit7',
+    en: '7',
+    ru: '7',
+    shiftEn: '&',
+    shiftRu: '?',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit8: {
+    code: 'Digit8',
+    en: '8',
+    ru: '8',
+    shiftEn: '*',
+    shiftRu: '*',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit9: {
+    code: 'Digit9',
+    en: '9',
+    ru: '9',
+    shiftEn: '(',
+    shiftRu: '(',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Digit0: {
+    code: 'Digit0',
+    en: '0',
+    ru: '0',
+    shiftEn: ')',
+    shiftRu: ')',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Minus: {
+    code: 'Minus',
+    en: '-',
+    ru: '-',
+    shiftEn: '_',
+    shiftRu: '_',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Equal: {
+    code: 'Equal',
+    en: '=',
+    ru: '=',
+    shiftEn: '+',
+    shiftRu: '+',
+    row: 1,
+    isTextKey: true,
+  },
+
+  Backspace: {
+    code: 'Backspace',
+    en: '&#8701; Backspace',
+    row: 1,
+    isTextKey: false,
+  },
+
+  Tab: {
+    code: 'Tab',
+    en: 'Tab &#11134;',
+    row: 2,
+    isTextKey: false,
+  },
+
+  KeyQ: {
+    code: 'KeyQ',
+    en: 'q',
+    ru: 'й',
+    shiftEn: 'Q',
+    shiftRu: 'Й',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyW: {
+    code: 'KeyW',
+    en: 'w',
+    ru: 'ц',
+    shiftEn: 'W',
+    shiftRu: 'Ц',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyE: {
+    code: 'KeyE',
+    en: 'e',
+    ru: 'у',
+    shiftEn: 'E',
+    shiftRu: 'У',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyR: {
+    code: 'KeyR',
+    en: 'r',
+    ru: 'к',
+    shiftEn: 'R',
+    shiftRu: 'К',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyT: {
+    code: 'KeyT',
+    en: 't',
+    ru: 'е',
+    shiftEn: 'T',
+    shiftRu: 'Е',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyY: {
+    code: 'KeyY',
+    en: 'y',
+    ru: 'н',
+    shiftEn: 'Y',
+    shiftRu: 'Н',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyU: {
+    code: 'KeyU',
+    en: 'u',
+    ru: 'г',
+    shiftEn: 'U',
+    shiftRu: 'Г',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyI: {
+    code: 'KeyI',
+    en: 'i',
+    ru: 'ш',
+    shiftEn: 'I',
+    shiftRu: 'Ш',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyO: {
+    code: 'KeyO',
+    en: 'o',
+    ru: 'щ',
+    shiftEn: 'O',
+    shiftRu: 'Щ',
+    row: 2,
+    isTextKey: true,
+  },
+
+  KeyP: {
+    code: 'KeyP',
+    en: 'p',
+    ru: 'з',
+    shiftEn: 'P',
+    shiftRu: 'З',
+    row: 2,
+    isTextKey: true,
+  },
+
+  BracketLeft: {
+    code: 'BracketLeft',
+    en: '[',
+    ru: 'х',
+    shiftEn: '{',
+    shiftRu: 'Х',
+    row: 2,
+    isTextKey: true,
+  },
+
+  BracketRight: {
+    code: 'BracketRight',
+    en: ']',
+    ru: 'ъ',
+    shiftEn: '}',
+    shiftRu: 'Ъ',
+    row: 2,
+    isTextKey: true,
+  },
+
+  Backslash: {
+    code: 'Backslash',
+    en: '&#92;',
+    ru: '&#92;',
+    shiftEn: '|',
+    shiftRu: '/',
+    row: 2,
+    isTextKey: true,
+  },
+
+  CapsLock: {
+    code: 'CapsLock',
+    en: 'Caps Lock',
+    row: 3,
+    isTextKey: false,
+  },
+
+  KeyA: {
+    code: 'KeyA',
+    en: 'a',
+    ru: 'ф',
+    shiftEn: 'A',
+    shiftRu: 'Ф',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyS: {
+    code: 'KeyS',
+    en: 's',
+    ru: 'ы',
+    shiftEn: 'S',
+    shiftRu: 'Ы',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyD: {
+    code: 'KeyD',
+    en: 'd',
+    ru: 'в',
+    shiftEn: 'D',
+    shiftRu: 'В',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyF: {
+    code: 'KeyF',
+    en: 'f',
+    ru: 'а',
+    shiftEn: 'F',
+    shiftRu: 'А',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyG: {
+    code: 'KeyG',
+    en: 'g',
+    ru: 'п',
+    shiftEn: 'G',
+    shiftRu: 'П',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyH: {
+    code: 'KeyH',
+    en: 'h',
+    ru: 'р',
+    shiftEn: 'H',
+    shiftRu: 'Р',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyJ: {
+    code: 'KeyJ',
+    en: 'j',
+    ru: 'о',
+    shiftEn: 'J',
+    shiftRu: 'О',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyK: {
+    code: 'KeyK',
+    en: 'k',
+    ru: 'л',
+    shiftEn: 'K',
+    shiftRu: 'Л',
+    row: 3,
+    isTextKey: true,
+  },
+
+  KeyL: {
+    code: 'KeyL',
+    en: 'l',
+    ru: 'д',
+    shiftEn: 'L',
+    shiftRu: 'Д',
+    row: 3,
+    isTextKey: true,
+  },
+
+  Semicolon: {
+    code: 'Semicolon',
+    en: ';',
+    ru: 'ж',
+    shiftEn: ':',
+    shiftRu: 'Ж',
+    row: 3,
+    isTextKey: true,
+  },
+
+  Quote: {
+    code: 'Quote',
+    en: '&#39;',
+    ru: 'э',
+    shiftEn: '"',
+    shiftRu: 'Э',
+    row: 3,
+    isTextKey: true,
+  },
+
+  Enter: {
+    code: 'Enter',
+    en: 'Enter',
+    row: 3,
+    isTextKey: false,
+  },
+
+  ShiftLeft: {
+    code: 'ShiftLeft',
+    en: '&#8657; Shift',
+    row: 4,
+    isTextKey: false,
+  },
+
+  KeyZ: {
+    code: 'KeyZ',
+    en: 'z',
+    ru: 'я',
+    shiftEn: 'Z',
+    shiftRu: 'Я',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyX: {
+    code: 'KeyX',
+    en: 'x',
+    ru: 'ч',
+    shiftEn: 'X',
+    shiftRu: 'Ч',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyС: {
+    code: 'KeyС',
+    en: 'с',
+    ru: 'с',
+    shiftEn: 'С',
+    shiftRu: 'С',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyV: {
+    code: 'KeyV',
+    en: 'v',
+    ru: 'м',
+    shiftEn: 'V',
+    shiftRu: 'М',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyB: {
+    code: 'KeyB',
+    en: 'b',
+    ru: 'и',
+    shiftEn: 'B',
+    shiftRu: 'И',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyN: {
+    code: 'KeyN',
+    en: 'n',
+    ru: 'т',
+    shiftEn: 'N',
+    shiftRu: 'Т',
+    row: 4,
+    isTextKey: true,
+  },
+
+  KeyM: {
+    code: 'KeyM',
+    en: 'm',
+    ru: 'ь',
+    shiftEn: 'M',
+    shiftRu: 'Ь',
+    row: 4,
+    isTextKey: true,
+  },
+
+  Comma: {
+    code: 'Comma',
+    en: ',',
+    ru: 'б',
+    shiftEn: '<',
+    shiftRu: 'Б',
+    row: 4,
+    isTextKey: true,
+  },
+
+  Period: {
+    code: 'Period',
+    en: '.',
+    ru: 'ю',
+    shiftEn: '>',
+    shiftRu: 'Ю',
+    row: 4,
+    isTextKey: true,
+  },
+
+  ArrowUp: {
+    code: 'ArrowUp',
+    en: '&#129093;',
+    row: 4,
+    isTextKey: false,
+  },
+
+  ShiftRight: {
+    code: 'ShiftRight',
+    en: '&#8657; Shift',
+    row: 4,
+    isTextKey: false,
+  },
+
+  ControlLeft: {
+    code: 'ControlLeft',
+    en: 'Ctrl',
+    row: 5,
+    isTextKey: false,
+  },
+
+  MetaLeft: {
+    code: 'MetaLeft',
+    en: 'Win',
+    row: 5,
+    isTextKey: false,
+  },
+
+  AltLeft: {
+    code: 'AltLeft',
+    en: 'Alt',
+    row: 5,
+    isTextKey: false,
+  },
+
+  Space: {
+    code: 'Space',
+    en: 'Space',
+    row: 5,
+    isTextKey: false,
+  },
+
+  AltRight: {
+    code: 'AltRight',
+    en: 'Alt',
+    row: 5,
+    isTextKey: false,
+  },
+
+  ArrowLeft: {
+    code: 'ArrowLeft ',
+    en: '&#129092;',
+    row: 5,
+    isTextKey: false,
+  },
+
+  ArrowDown: {
+    code: 'ArrowDown ',
+    en: '&#129095;',
+    row: 5,
+    isTextKey: false,
+  },
+
+  ArrowRight: {
+    code: 'ArrowRight ',
+    en: '&#129094;',
+    row: 5,
+    isTextKey: false,
+  },
+
+  ControlRight: {
+    code: 'ControlRight ',
+    en: 'Ctrl;',
+    row: 5,
+    isTextKey: false,
+  },
+};
+
+export default keys;
